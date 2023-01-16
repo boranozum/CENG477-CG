70d1ede0bff7fe6a4bb92bfaa20522b287a73ab1
AHMET BORAN OZUM <e2380814@newinek>
2023-01-16 21:11:15 +0300

    kaydirma

--- HW3/basic_shader_glfw_model/main.cpp
+++ HW3/basic_shader_glfw_model/main.cpp
@@ -62,11 +62,24 @@ class Cell
         float size;
         float explode_size;
         bool will_explode;
+        bool exploded;
         Cell(GLfloat inX, GLfloat inY, GLint inColor_index) : x(inX), y(inY), color_index(inColor_index){ 
             size = 25.0/48.0;
             explode_size = size*1.5;
             will_explode = false;
+            exploded = false;
         }
+        // do assignment operator overloading
+        Cell& operator=(const Cell& rhs) {
+            x = rhs.x;
+            y = rhs.y;
+            color_index = rhs.color_index;
+            size = rhs.size;
+            explode_size = rhs.explode_size;
+            will_explode = rhs.will_explode;
+            exploded = rhs.exploded;
+            return *this;
+        }   
 
 };
 
@@ -485,20 +498,45 @@ void display()
             GLint color = glGetUniformLocation(gProgram, "colorIndex");
             glUniform1i(color, grid[i][j].color_index);
 
-            // findMatches(grid, grid_w, grid_h);
+            //findMatches();
 
-            if(grid[i][j].will_explode){
+            if(grid[i][j].will_explode && !grid[i][j].exploded){
                 if(grid[i][j].size <= grid[i][j].explode_size){
                     grid[i][j].size += grid[i][j].size*0.01;
                     glScalef(grid[i][j].size, grid[i][j].size, grid[i][j].size); 
                 }
                 else{
-                    grid[i][j].size = 25.0/48.0;
+                    // grid[i][j].size = 25.0/48.0;
                     grid[i][j].will_explode = false;
+                    grid[i][j].exploded = true;
                 }
             }
-            else{
+            else if (!grid[i][j].exploded){
                 glScalef(grid[i][j].size, grid[i][j].size, grid[i][j].size); 
+            }
+            else{
+                if(j != grid_h-1){
+                    for (int k = j+1; k < grid_h; k++)
+                    {
+                        if(grid[i][k].y != grid[i][k-1].y){
+                            grid[i][k].y = grid[i][k].y - 0.05;
+                        }    
+
+                        if(grid[i][k].y >= grid[i][k-1].y - 0.05 && grid[i][k].y <= grid[i][k-1].y + 0.05){
+                            grid[i][k].y = grid[i][k-1].y;
+                            grid[i][j].exploded = false;
+                            for(int l = j; l < grid_h; l++){
+                                grid[i][l] = grid[i][l+1];
+                            }
+                            grid[i][grid_h-1] = Cell(-10+(20.0/(grid_w+1))+20*i/(grid_w+1), (-10+(20.0/(grid_h+1))+20*grid_h/(grid_h+1)), rand() % 5);
+                            
+                        }
+                    }
+                }
+                continue;
+                // else{
+                    
+                // }
             }
 
             drawModel();
